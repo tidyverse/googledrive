bc10669e0d86b62910de4abb649d45ce84ce598f
Lucy <ld.mcgowan@vanderbilt.edu>
2017-04-30 17:37:57 -0500

    allow regex in ls

--- R/gd_ls.R
+++ R/gd_ls.R
@@ -1,9 +1,12 @@
 #' Google Drive List
 #'
+#' @param search character, regular expression(s) of title(s) of documents to output in a tibble. If it is \code{NULL} (defualt), information about all documents in drive will be output in a tibble.
+#' @param verbose Logical, indicating whether to print informative messages (default \code{TRUE})
+#'
 #' @return tibble of files
 #' @export
 #'
-gd_ls <- function(){
+gd_ls <- function(search = NULL, ..., verbose = TRUE){
   token <- .state$token
   req <- httr::GET(.state$gd_base_url_files_v3,token)
   httr::stop_for_status(req)
@@ -15,5 +18,24 @@ gd_ls <- function(){
     type = sub('.*\\.', '',purrr::map_chr(reqlist$files, "mimeType")),
     id = purrr::map_chr(reqlist$files, "id")
   )
-  req_tbl
+
+  if (is.null(search)){
+
+    return(req_tbl)
+
+  } else{
+
+    stopifnot(inherits(search, "character"))
+
+  }
+
+  keep_names <- grep(search, req_tbl$name, ...)
+
+  if(length(keep_names) == 0L){
+    if(verbose){
+      message(paste0("We couldn't find any documents matching '", search, "'. Try updating your search critria."))
+    }
+    invisible(NULL)
+  } else
+    req_tbl[keep_names,]
 }
