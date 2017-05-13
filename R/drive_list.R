743520a090b47d1b8704e5a33a0ef73299905fcb
jennybc <jenny@stat.ubc.ca>
2017-05-13 14:24:39 -0700

    Partial reworking of get_leafmost_id()

--- R/drive_list.R
+++ R/drive_list.R
@@ -89,46 +89,63 @@ drive_list <- function(path = NULL, pattern = NULL, ..., verbose = TRUE){
 }
 
 get_leafmost_id <- function(path) {
-  folder_nms <- unlist(strsplit(path, "/"))
-  folder_pattern <- paste0("^", folder_nms, "$", collapse = "|")
-  folder_order <- tibble::tibble(
-    name = folder_nms,
-    dir = seq_along(folder_nms)
-  )
-  folder_tbl <- drive_list(pattern = folder_pattern,
-                           fields = "files/parents,files/name,files/mimeType,files/id",
-                           q = "mimeType='application/vnd.google-apps.folder'")
 
-  if (!all(folder_nms %in% folder_tbl$name)){
+  path_pieces <- tibble::tibble(
+    name =  unlist(strsplit(path, "/")),
+    depth = seq_along(name)
+  )
+  d <- max(path_pieces$depth)
+  path_pattern <- paste0("^", path_pieces$name, "$", collapse = "|")
+  folders <- drive_list(
+    pattern = path_pattern,
+    fields = "files/parents,files/name,files/mimeType,files/id",
+    q = "mimeType='application/vnd.google-apps.folder'"
+  )
+  ## FIXME
+  ## seems like the tibble returned above should have a "parents" variable?
+  ## and that it should be a list-column, not character
+  ## can't parents technically have length greater than one?
+  ## temporarily just fixing the name
+  folders$parents <- folders$folder_id
+  folders$folder_id <- NULL
+
+  if (!all(path_pieces$name %in% folders$name)){
     spf("We could not find the file path '%s' on your Google Drive", path)
   }
+  ## guarantee: we have found at least one folder with correct name for
+  ## each piece of path
 
-  # merge in to get the correct directory order & sort
-  folder_tbl <- folder_tbl %>%
-    merge(folder_order, by = "name")
-  folder_tbl_sort <- folder_tbl[order(folder_tbl$dir),]
-    # I could do in a pipe with `[`(order(.$dir), ) is this kosher?
+  folders <- tibble::as_tibble(merge(folders, path_pieces, by = "name"))
+  folder <- folders$id[folders$depth == d]
+  if (length(folder) != 1) {
+    spf("'%s' does not uniquely define a single path", path)
+  }
+  ## guarantee: there is exactly one folder at depth d
 
+  ## the only task left is to make sure you can get from folder to root
+  ## by traversing a child-->parent chain of relationships
+  ## once that's established, you know folder is good and you return it
+
+  ## FIXME: this is too simple, I think this will have to be recursive
+  ## example that breaks it: path = foo/bar/baz
+  ## foo/bar/baz DOES exist
+  ## but there is a second folder named bar under foo
+  folders <- folders[order(folders$depth), ]
   parent_id <- root_folder()
   keep_folders <- NULL
-  for (i in 1:nrow(folder_tbl_sort)){
-    subfolder <- folder_tbl_sort[i, ]
-    keep <- subfolder$folder_id == parent_id
-    if (keep){
+  for (i in seq_len(nrow(folders))) {
+    subfolder <- folders[i, ]
+    keep <- subfolder$parents == parent_id
+    if (keep) {
       parent_id <- subfolder$id
     }
     keep_folders[i] <- keep
   }
 
-  folder_tbl_keep <- folder_tbl_sort[keep_folders, ]
-  leafmost <- nrow(folder_tbl_keep)
-  if (leafmost != length(folder_nms)){
-    spf("Uh oh, something went wrong. We were unable to find the folder '%s' within the path '%s'.",
-        folder_nms[leafmost + 1],
-        paste0(folder_nms[1:leafmost], collapse = "/"))
+  if (!keep_folders[length(keep_folders)]) {
+    spf("Path not found: '%s'", path)
   }
-
-  folder_tbl_keep[leafmost, "id"]
+  folder
 }
 
 ## gets the root folder id
