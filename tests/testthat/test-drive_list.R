6a0e39a5a68b7a9dd3998c1afcb4a38ee6f9495a
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-14 19:16:01 -0500

    add more tests

--- tests/testthat/test-drive_list.R
+++ tests/testthat/test-drive_list.R
@@ -1,5 +1,6 @@
 internet = FALSE
-test_that("drive_test when we have 2 folders of the same name & depth", {
+#internet = TRUE
+test_that("drive_list when we have 2 folders of the same name & depth", {
   skip_if_not(internet)
 
   url <- .drive$base_url_files_v3
@@ -28,4 +29,44 @@ test_that("drive_test when we have 2 folders of the same name & depth", {
   ids <- list(foo_id, yo_id)
   cleanup <- purrr::map(ids, drive_file) %>%
     purrr::map(drive_delete)
+})
+
+test_that("drive_list when we have two folders of the same name in the same location (and one has the correct output)", {
+  ## path = foo/bar/baz
+  ## foo/bar/baz DOES exist
+  ## but there are two folders named bar under foo, one of which hosts baz
+
+  foo_id <- drive_mkdir("foo")$id
+  ## bar is in foo
+  bar_id <- drive_mkdir("bar", path = "foo")$id
+  ## baz is in foo/bar
+  baz_id <- drive_mkdir("baz", path = "foo/bar")$id
+  ## let's stick something in baz to know what to look for
+  yo_id <- drive_mkdir("yo", path = "foo/bar/baz")$id
+  ## there is another bar in foo (without baz in it)
+  bar_2_id <- drive_mkdir("bar", path = "foo")$id
+
+  ## let's look in foo, there should be two folders named "bar"
+  expect_true(all(c(bar_id, bar_2_id) %in% drive_list(path = "foo")$id))
+
+  ## let's try to see if the function can find "baz" in the correct foo/bar
+  ## (this should output yo)
+  expect_identical(yo_id, drive_list(path = "foo/bar/baz")$id)
+
+  ## clean up
+  foo_id %>%
+    drive_file %>%
+    drive_delete
+
+})
+test_that("drive_list when we have two folders of the same name in the same location", {
+  ## Google Drive treats folders like labels, so you can have two folders with the
+  ## exact same name in the same location. This is silly. At them moment, if you try
+  ## to search within a folder like this, we will throw you an error
+
+  ## create two folders named foo in the root directory
+  foo1 <- drive_mkdir("foo")
+  foo2 <- drive_mkdir("foo")
+
+
 })
