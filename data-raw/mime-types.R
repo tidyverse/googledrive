48c88b5c0463eb212c798f9c80de078f6f388f05
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-15 19:05:18 -0500

    create tbls of mimeTypes (#19)

--- data-raw/mime-types.R
+++ data-raw/mime-types.R
@@ -1,3 +1,32 @@
 #mime-types tables
 #https://developers.google.com/drive/v3/web/mime-types
 #https://developers.google.com/drive/v3/web/manage-downloads
+library('dplyr')
+
+url <- "https://developers.google.com/drive/v3/web/manage-downloads"
+
+download_mime_types <- httr::GET(url) %>%
+  httr::content() %>%
+  rvest::html_table(fill = TRUE) %>%
+  purrr::flatten() %>%
+  tibble::as_tibble() %>%
+  filter(rowSums(is.na(.)) != ncol(.)) %>%  #remove NAs
+  mutate(google_fmt = replace(`Google Doc Format`,
+                              `Google Doc Format` == "", NA)) %>%
+  select(google_fmt,
+         conversion_fmt = `Conversion Format`,
+         mime_type = `Corresponding MIME type`
+  ) %>%
+  do(zoo::na.locf(.)) #fill
+
+url <- "https://developers.google.com/drive/v3/web/mime-types"
+query_mime_types <- httr::GET(url) %>%
+  httr::content() %>%
+  rvest::html_table(fill = TRUE) %>%
+  purrr::flatten() %>%
+  tibble::as_tibble() %>%
+  select(mime_type = `MIME Type`,
+         google_fmt = `Description`)
+
+#.drive$query_mime_types <- query_mime_types
+#.drive$download_mime_types <- download_mime_types
