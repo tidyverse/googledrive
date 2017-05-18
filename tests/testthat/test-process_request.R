839957c49df367637eb3e097729fa07342a4ea69
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-18 16:35:29 -0500

    split tests, change default url, switch to expect_false()

--- tests/testthat/test-process_request.R
+++ tests/testthat/test-process_request.R
@@ -1,4 +1,4 @@
-test_that("process_request properly errors on wrong content type", {
+test_that("process_request properly errors on wrong content type with default expected type", {
 
   ## create fake res with the wrong content type
   actual <- "test"
@@ -11,8 +11,10 @@ test_that("process_request properly errors on wrong content type", {
                "application/json; charset=UTF-8",
                actual)
   )
+})
 
-  ## let's try if I feed it an expected type
+test_that("process_request properly errors on wrong content type with inserted expected type", {
+
   actual = "test"
   expected = "something else"
   res <- list(headers = list("content-type" = actual))
@@ -24,15 +26,18 @@ test_that("process_request properly errors on wrong content type", {
                expected,
                actual)
   )
+})
+test_that("process_request is okay with correct content type and default expected type", {
 
-  ## let's try it if it's okay
   actual = "application/json; charset=UTF-8"
   res <- list(headers = list("content-type" = actual))
 
   ## should return NULL
   expect_equal(process_request(res, internet = FALSE), NULL)
+})
 
-  ## also if we set a new expectation
+test_that("process_request is okay with correct content type and given expected type", {
+
   actual = "test"
   expected = "test"
   res <- list(headers = list("content-type" = actual))
@@ -47,18 +52,18 @@ test_that("process_request properly errors on wrong content type", {
 
 ## testing on the API
 
-test_that("process_request properly errors on wrong content type", {
+test_that("process_request properly errors on wrong content type (API)", {
 
   skip_on_appveyor()
   skip_on_travis()
 
   ## something we know won't be correct
-  res <- httr::GET("http://livefreeordichotomize.com")
+  res <- httr::GET("https://httpbin.org")
 
   expected = "application/json; charset=UTF-8"
   actual = res$headers$`content-type`
   ## let's make sure our test is actually good, this should be true
-  expect_true(actual != expected)
+  expect_false(identical(actual, expected))
 
   expect_error(process_request(res),
                sprintf(paste0("Expected content-type:\n%s",
@@ -68,19 +73,4 @@ test_that("process_request properly errors on wrong content type", {
                expected,
                actual)
   )
-
-  x <- drive_list()[1,] # grab most recent thing on drive
-
-  ## should just pull the name
-  url <- httr::modify_url(url = .drive$base_url,
-                          path = paste0("drive/v3/files/",x$id),
-                          query = list(fields = "name"))
-
-  res <- httr::GET(url, drive_token())
-
-  ## this should just be a list 2 one element, name
-  proc_res <- process_request(res)
-  expect_equal(proc_res, list(name = x$name))
-
 })
-
