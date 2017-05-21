6c3fb5e9d7c5037f2242c2e8cedb959cb772e364
Lucy <ld.mcgowan@vanderbilt.edu>
2017-05-21 17:31:37 -0500

    try to fix appveyor

--- tests/testthat/test-drive_list.R
+++ tests/testthat/test-drive_list.R
@@ -6,7 +6,7 @@ context("List files")
 
 ## ad hoc code for cleaning up if tests exit uncleanly
 # (pesky_files <- drive_list(pattern = "foo|bar|baz|yo"))
-# pesky_files$id %>% purrr::map(drive_file) %>% purrr::map(drive_delete)
+# dpesky_files$id %>% purrr::map(drive_file) %>% purrr::map(drive_delete)
 
 test_that("drive_list when we have 2 folders of the same name & depth", {
   skip_on_appveyor()
