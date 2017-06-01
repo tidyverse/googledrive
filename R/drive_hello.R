53a38f79081f999c1d419f9de15fc482d1e5bb9e
jennybc <jenny@stat.ubc.ca>
2017-05-31 22:29:02 -0700

    Make a demo fxn, drive_hello()

---
+++ R/drive_hello.R
@@ -0,0 +1,16 @@
+#' @export
+drive_hello <- function(file) {
+  drib <- as.dribble(file)
+  ## this silly second pass is necessary because, eg, drive_path() doesn't
+  ## actually return a dribble yet! but you get the idea, yes?
+  drib <- as.dribble(drive_id(drib$id))
+  message(
+    glue::glue_data(
+      drib,
+      "Hi, my name is {sq(name)}!",
+      "My fileId starts with {substr(id, 1, 7)}",
+      .sep = "\n"
+    )
+  )
+  invisible(drib)
+}
