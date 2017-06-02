7c8c8b23ae22badad26c912f6c9569d0328d802f
jennybc <jenny@stat.ubc.ca>
2017-06-01 23:40:24 -0700

    Tweak drive_browse()

--- R/drive_browse.R
+++ R/drive_browse.R
@@ -1,16 +1,18 @@
-#' Open Google Drive file in browser to edit.
+#' Visit Google Drive file in browser.
 #'
 #' @template file
 #'
 #' @template dribble-return
 #' @export
-drive_browse <- function(file){
+drive_browse <- function(file) {
   if (!interactive()) return(invisible(file))
 
   file <- as_dribble(file)
 
+  ## TO DO: do we really want to require a 1 row dribble?
+  ## another options is to browse first n where n is low, maybe even 1
   if (nrow(file) != 1) {
-    spf("Input `file` must be a `dribble` with 1 row.")
+    spf("Input must specify exactly 1 Drive file.")
   }
 
   link <- drive_share_link(file = file)
