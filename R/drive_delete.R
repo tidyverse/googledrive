27de6cd860bdd1dba3889fe56510cef573c25825
jennybc <jenny@stat.ubc.ca>
2017-06-01 23:56:40 -0700

    Groom drive_delete()

--- R/drive_delete.R
+++ R/drive_delete.R
@@ -7,12 +7,16 @@
 #' @export
 #'
 drive_delete <- function(file = NULL, verbose = TRUE) {
-
   file <- as_dribble(file)
+  out <- purrr::map2_lgl(file$id, file$name, delete_one, verbose = verbose)
+  invisible(out)
+}
 
-  out <- purrr::map2_lgl(file$id, file$name, ~ {
-  request <- build_request(endpoint = "drive.files.delete",
-                           params = list(fileId = .x))
+delete_one <- function(id, name, verbose = TRUE) {
+  request <- build_request(
+    endpoint = "drive.files.delete",
+    params = list(fileId = id)
+  )
   response <- make_request(request)
 
   ## note, delete does not send a response header, so we
@@ -21,23 +25,12 @@ drive_delete <- function(file = NULL, verbose = TRUE) {
 
   httr::stop_for_status(response)
 
-  if (verbose == TRUE) {
-    if (response$status_code == 204L) {
-      message(
-        glue::glue(
-        "The file '{.y}' has been deleted from your Google Drive"
-        )
-      )
+  if (verbose) {
+    if (httr::status_code(response) == 204L) {
+      message(glue::glue("File deleted from Google Drive:\n{name}"))
     } else {
-      message(
-        glue::glue(
-        "Zoinks! Something went wrong, '{.y}' was not deleted."
-        )
-      )
+      message(glue::glue("Zoinks! File NOT deleted:\n{name}"))
     }
   }
-
-  if (response$status_code == 204L) TRUE else FALSE
-  })
-  invisible(out)
+  response$status_code == 204L
 }
--- tests/testthat/test-drive_share.R
+++ tests/testthat/test-drive_share.R
@@ -12,28 +12,29 @@ test_that("drive_share doesn't explicitly fail", {
   write.table(chickwts, "chickwts.txt")
   on.exit(unlink("chickwts.txt"))
 
-  drive_chickwts <- drive_upload("chickwts.txt",
-                                 up_name = nm_("chickwts"),
-                                 verbose = FALSE)
+  drive_chickwts <- drive_upload(
+    "chickwts.txt",
+    up_name = nm_("chickwts"),
+    verbose = FALSE
+  )
   ## since we haven't updated the permissions, the permissions
   ## tibble should be just 1 row
-  expect_equal(length(drive_chickwts$files_resource[[1]]$permissions), 1)
+  expect_length(drive_chickwts$files_resource[[1]]$permissions, 1)
 
   role <- "reader"
   type <- "anyone"
   ## it should tell me they've been shared
-  expect_message({
+  expect_message(
     drive_chickwts <- drive_share(
       drive_chickwts,
       role = role,
       type = type
-    )
-  },
-  glue::glue("The permissions for file '{drive_chickwts$name}' have been updated")
+    ),
+    glue::glue("The permissions for file {sq(drive_chickwts$name)} have been updated")
   )
 
   ## this new drive_chickwts should have a larger tibble
-  expect_equal(length(drive_chickwts$files_resource[[1]]$permissions), 2)
+  expect_length(drive_chickwts$files_resource[[1]]$permissions, 2)
 
   ## this new tibble should have type "user" and the type
   ## defined above, and the roles should be "owner" and
