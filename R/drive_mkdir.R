9623639ba4d7b8ecb6898632a54d6f78636ecf4f
jennybc <jenny@stat.ubc.ca>
2017-06-02 00:13:03 -0700

    Groom drive_mkdir()

--- R/drive_mkdir.R
+++ R/drive_mkdir.R
@@ -1,7 +1,7 @@
 #' Create a folder on Google Drive.
 #'
-#' @param dir A character string. The name of the folder you would like to create.
-#' @param path A character string. The path where you would like the folder on your Google Drive.
+#' @param dir Character. The name of the folder you would like to create.
+#' @param path Character string. The path of the parent folder.
 #' @template verbose
 #'
 #' @template dribble-return
@@ -9,6 +9,8 @@
 drive_mkdir <- function(dir = NULL, path = NULL, verbose = TRUE) {
   parent <- NULL
 
+  ## TO DO: accept path input in the generalized "Drive file" style
+  ## and check that it's a folder
   if (!is.null(path)) {
     path <- append_slash(path)
     parent <- get_one_path(path)$id
@@ -26,24 +28,16 @@ drive_mkdir <- function(dir = NULL, path = NULL, verbose = TRUE) {
   response <- make_request(request, encode = "json")
   proc_res <- process_response(response)
 
+  ## LUCY: why do we call the API again?
   folder <- as_dribble(drive_id(proc_res$id))
 
   success <- folder$name == dir
   if (verbose) {
     if (success) {
-      message(
-        glue::glue_data(
-          folder,
-          "You have successfully uploaded the folder: '{name}'."
-          )
-        )
-    } else
-      message(
-        glue::glue_data(
-          folder,
-          "Uh oh, something went wrong. The folder '{name}' was not uploaded."
-        )
-      )
+      message(glue::glue_data(folder, "Folder created:\n{name}"))
+    } else {
+      message(glue::glue_data(folder, "Uh oh, folder NOT created:\n{name}"))
+    }
   }
   invisible(folder)
 }
