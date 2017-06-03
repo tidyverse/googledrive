0a6ac0bf5c97e36ff9b8e9b595f8a905771e5905
Lucy <ld.mcgowan@vanderbilt.edu>
2017-06-03 09:43:00 -0500

    update drive_ls() to take any input

--- R/drive_ls.R
+++ R/drive_ls.R
@@ -19,15 +19,22 @@
 #' drive_ls(path = "abc", pattern = "def")
 #' }
 drive_ls <- function(path = "~/", pattern = NULL, ...) {
-  path <- append_slash(path)
-  path <- drive_path(path)
-
-  path <- is_one(path)
 
   x <- list(...)
-  q_clause <- paste(sq(path$id), "in parents")
-  if (!is.null(x$q)) {
-    q_clause <- paste(x$q, "and", q_clause)
+  q_clause <- NULL
+
+  if (!is.null(path)) {
+    if (is.character(path)) {
+      path <- append_slash(path)
+    }
+    path <- as_dribble(path)
+    path <- is_one(path)
+
+    q_clause <- paste(sq(path$id), "in parents")
+    if (!is.null(x$q)) {
+      q_clause <- paste(x$q, "and", q_clause)
+    }
   }
+
   drive_search(pattern = pattern, q = q_clause)
 }
