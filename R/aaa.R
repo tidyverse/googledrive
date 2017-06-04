b465f3872123239dda62cf409bf2169ab082638b
jennybc <jenny@stat.ubc.ca>
2017-06-04 14:35:13 -0700

    Split into aaa.R and utils.R

--- R/aaa.R
+++ R/aaa.R
@@ -1,13 +1,3 @@
-#' Pipe operator
-#'
-#' @name %>%
-#' @rdname pipe
-#' @keywords internal
-#' @export
-#' @importFrom purrr %>%
-#' @usage lhs \%>\% rhs
-NULL
-
 # environment to hold data about the Drive API
 .drive <- new.env(parent = emptyenv())
 .drive$base_url <- "https://www.googleapis.com"
@@ -47,47 +37,4 @@ NULL
 
   invisible()
 
-}
-
-## from gh
-
-has_names <- function(x){
-  n <- names(x)
-  if (is.null(n)){
-    rep_len(FALSE, length(x))
-  } else{
-    !(is.na(n) | n == "")
-  }
-}
-
-has_no_names <- function(x) all(!has_names(x))
-
-clean_names <- function(x){
-  if (has_no_names(x)){
-    names(x) <- NULL
-  }
-  x
-}
-
-# fx
-spf <- function(...) stop(sprintf(...), call. = FALSE)
-
-last <- function(x) x[length(x)]
-
-sq <- function(x) glue::single_quote(x)
-
-## removes last abs(n) elements
-crop <- function(x, n = 6L) if (n == 0) x else utils::head(x, -1 * abs(n))
-
-## version of glue::collapse() that returns x if length(x) == 0
-collapse2 <- function(x, sep = "", width = Inf, last = "") {
-  if (length(x) == 0) {
-    x
-  } else
-    glue::collapse(x = x, sep = sep, width = width, last = last)
-}
-
-## finds max i such that all(x[1:i])
-last_all <- function(x) {
-  Position(isTRUE, as.logical(cumprod(x)), right = TRUE, nomatch = 0)
 }
