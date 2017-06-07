d1a77268ac96d4f7d291902976723e2fdefe99a0
Lucy <ld.mcgowan@vanderbilt.edu>
2017-06-07 00:22:43 -0500

    first pass at discovery docs :rooster:

---
+++ R/generate_request.R
@@ -0,0 +1,217 @@
+#' Generate a request for the Google Drive v3 API
+#'
+#' Generate a request, using some knowledge of the [Drive v3
+#' API](https://developers.google.com/drive/v3/web/about-sdk). Most users
+#' should, instead, use higher-level wrappers that facilitate common tasks, such
+#' as reading the contents of a sheet. The functions here are intended for
+#' internal use and for programming around the Sheets API.
+#'
+#' There are two functions:
+#' * `generate_request()` is a wrapper around `build_request()`. It takes
+#' a nickname for an endpoint and uses the API spec to look up the `path` and
+#' `method`. The `params` are checked for validity and completeness with respect
+#' to the endpoint. It then passes things along to `build_request()`.
+#' * `build_request()` builds a request from explicit parts. It is quite
+#' dumb, only doing URL endpoint substitution and URL formation. It's up to the
+#' caller to make sure the `path`, `method`, and `params` are valid.
+#'
+#' @param endpoint `character(1)`\cr Nickname for one of the documented Drive
+#'   v3 API endpoints. *to do: list or link, once I've auto-generated those
+#'   docs*
+#' @param params `named list()`\cr Parameters destined for endpoint URL
+#'   substitution or, otherwise, the query.
+#' @param .api_key NULL for now..
+#'
+
+#' @return `list()`\cr Components are `method`, `path`, `query`, and `url`,
+#'   suitable as input for [gs_make_request()]. The `path` is post-substitution
+#'   and the `query` is a named list of all the input `params` that were not
+#'   used during this substitution. `url` is the full URL after prepending the
+#'   base URL for the Drive v3 API and appending an API key to the query.
+#' @export
+#' @examples
+#' req <- generate_request(
+#'   "files.get",
+#'   list(
+#'     fileId = "abc",
+#'   )
+#' )
+#' req
+generate_request <- function(endpoint = character(),
+                                params = list(),
+                                .api_key = NULL) {
+  ept <- .endpoints[[endpoint]]
+  if (is.null(ept)) {
+    stop("Endpoint not recognized:\n", endpoint, call. = FALSE)
+  }
+
+  ## use the spec to vet and rework request parameters
+  params <-   match_params(params, ept$parameters)
+  params <- handle_repeats(params, ept$parameters)
+  check_enums(params, ept$parameters)
+
+  drive_build_request(
+    path = ept$path,
+    method = ept$method,
+    params = params,
+    .api_key = .api_key
+  )
+}
+
+#' @param path character, e.g.,
+#'   `"/files/{fileId}/"`. It can include
+#'   variables inside curly brackets, as the example does, which are substituted
+#'   using named parameters found in the `params` argument.
+#' @param method character, should match an HTTP verb, e.g., `GET`, `POST`, or
+#'   `PUT`
+#' @rdname generate_request
+#' @export
+#' @examples
+#' req <- build_request(
+#'   path = "files/{fileId}",
+#'   method = "GET",
+#'   list(
+#'     fileId = "abc",
+#'   )
+#' )
+#' req
+drive_build_request <- function(path,
+                          method,
+                          params = list(),
+                          .api_key = NULL) {
+  params <- partition_params(params, extract_param_names(path))
+  out <- list(
+    method = method,
+    path = paste0("drive/v3/", glue::glue_data(params$path_params, path)),
+    query = c(params$query_params)
+  )
+  ## yes it is redundant to store the parts and the url
+  ## but it is helpful to me, so it shall be this way
+  out$url <- httr::modify_url(
+    url = .drive$base_url,
+    path = out$path,
+    query = out$query
+  )
+  out
+}
+
+## match params provided by user to spec
+##   * error if required params are missing
+##   * message and drop unknown params
+match_params <- function(provided, spec) {
+  ## .endpoints %>% map("parameters") %>% flatten() %>% map_lgl("required")
+  required <- spec %>% purrr::keep("required") %>% names()
+  missing <- setdiff(required, names(provided))
+  if (length(missing)) {
+    stop("Required parameter(s) are missing:\n", missing, call. = FALSE)
+  }
+
+  unknown <- setdiff(names(provided), names(spec))
+  if (length(unknown)) {
+    m <- names(provided) %in% unknown
+    msgs <- c(
+      "Ignoring these unrecognized parameters:",
+      glue::glue_data(tibble::enframe(provided[m]), "{name}: {value}")
+    )
+    message(paste(msgs, collapse = "\n"))
+    provided <- provided[!m]
+  }
+  return(provided)
+}
+
+## certain params can be repeated on specific endpoints, e.g., ranges
+##   * replicate as needed in the query params
+##   * detect and error for any other repetition
+handle_repeats <- function(provided, spec) {
+
+  if (length(provided) == 0) {
+    return(provided)
+  }
+  can_repeat <- spec[names(provided)] %>%
+    purrr::map_lgl("repeated") %>%
+    purrr::map_lgl(isTRUE)
+  too_long <- lengths(provided) > 1 & !can_repeat
+  if (any(too_long)) {
+    stop(
+      "These parameter(s) are not allowed to have length > 1:\n",
+      names(provided)[too_long],
+      call. = FALSE
+    )
+  }
+
+  is_a_repeat <- duplicated(names(provided))
+  too_many <- is_a_repeat & !can_repeat
+  if (any(too_many)) {
+    stop(
+      "These parameter(s) are not allowed to appear more than once:\n",
+      names(provided)[too_many],
+      call. = FALSE
+    )
+  }
+
+  ## replicate anything with length > 1
+  n <- lengths(provided)
+  nms <- names(provided)
+  ## this thwarts protection from urlencoding via I() ... revisit if needed
+  provided <- provided %>% purrr::flatten() %>% purrr::set_names(rep(nms, n))
+
+  return(provided)
+}
+
+## a few parameters have fixed lists of possible values -- a.k.a the "enums"
+check_enums <- function(provided, spec) {
+  values <- spec %>% purrr::map("enum")
+  if (length(provided) == 0 | length(values) == 0) {
+    return(provided)
+  }
+  check_it <- tibble::tibble(
+    pname = names(provided),
+    pdata = purrr::flatten_chr(provided)
+  )
+  check_it$values = values[check_it$pname]
+  not_an_enum <- check_it$values %>% purrr::map(is.na) %>% purrr::map_lgl(all)
+  check_it <- check_it[!not_an_enum, ]
+  ok <- purrr::map2_lgl(check_it$pdata, check_it$values, ~ .x %in% .y)
+  if (any(!ok)) {
+    problems <- check_it[!ok, ]
+    problems$values <- problems$values %>% purrr::map_chr(paste, collapse = " | ")
+    template <- paste0("Parameter '{pname}' has value '{pdata}', ",
+                       "but it must be one of these:\n{values}\n\n")
+    msgs <- glue::glue_data(problems, template)
+    msgs %>% purrr::walk(message)
+    stop("Invalid parameter value(s).", call. = FALSE)
+  }
+  return(provided)
+}
+
+## extract the path params by name and put the leftovers in query
+## why is this correct?
+## if the endpoint was specified, we have already matched against spec
+## if the endpoint was unspecified, we have no choice
+partition_params <- function(provided, path_param_names) {
+  query_params <- provided
+  path_params <- NULL
+  if (length(path_param_names) && length(query_params)) {
+    m <- names(provided) %in% path_param_names
+    path_params <- query_params[m]
+    query_params <- query_params[!m]
+  }
+  ## if no query_params, NULL is preferred to list() for the sake of
+  ## downstream URLs, though the API key will generally imply there are
+  ## no empty queries
+  if (length(query_params) == 0) {
+    query_params <- NULL
+  }
+  return(list(
+    path_params = path_params,
+    query_params = query_params
+  ))
+}
+
+##  input: /v4/spreadsheets/{spreadsheetId}/sheets/{sheetId}:copyTo
+## output: spreadsheetId, sheetId
+extract_param_names <- function(path) {
+  m <- gregexpr("\\{[^/]*\\}", path)
+  path_param_names <- regmatches(path, m)[[1]]
+  gsub("[\\{\\}]", "", path_param_names)
+}
---
+++ data-raw/discovery-doc-prep.R
@@ -0,0 +1,213 @@
+library(rprojroot)
+library(jsonlite)
+library(httr)
+library(tidyverse)
+library(stringr)
+
+## load the API spec, including download if necessary
+dd_cache <- find_package_root_file("data-raw") %>%
+  list.files(pattern = "discovery-document.json$", full.names = TRUE)
+if (length(dd_cache) == 0) {
+  dd_get <- GET("https://www.googleapis.com/discovery/v1/apis/drive/v3/rest")
+  dd_content <- content(dd_get)
+  json_fname <- dd_content[c("revision", "id")] %>%
+    c("discovery-document") %>%
+    map(~ str_replace_all(.x, ":", "-")) %>%
+    str_c(collapse = "_") %>%
+    str_c(".json") %>%
+    find_package_root_file("data-raw", .)
+  write_lines(
+    content(dd_get, as = "text"),
+    json_fname
+  )
+} else {
+  json_fname <- rev(dd_cache)[1]
+}
+dd_content <- fromJSON(json_fname)
+##view(dd_content)
+listviewer::jsonedit(dd_content)
+
+## extract the method collections and bring to same level of hierarchy
+files <- dd_content[[c("resources",  "files", "methods")]]
+names(files) <- paste("files", names(files), sep = ".")
+permissions <-
+  dd_content[[c("resources", "permissions", "methods")]]
+names(permissions) <- paste("permissions", names(permissions), sep = ".")
+revisions <-
+  dd_content[[c("resources", "revisions", "methods")]]
+names(revisions) <- paste("revisions", names(revisions), sep = ".")
+
+## extract the schema collections and bring to same level of hierarchy
+files_schema <- dd_content[[c("schemas",  "File", "properties")]]
+permissions_schema <-
+  dd_content[[c("schemas", "Permission", "properties")]]
+revisions_schema <-
+  dd_content[[c("schemas", "Revision", "properties")]]
+
+
+endpoints <- c(files, permissions, revisions)
+# str(endpoints, max.level = 1)
+listviewer::jsonedit(endpoints)
+
+nms <- endpoints %>%
+  map(names) %>%
+  reduce(union)
+
+## tibble with one row per endpoint
+edf <- endpoints %>%
+  transpose(.names = nms) %>%
+  simplify_all(.type = character(1)) %>%
+  as_tibble()
+View(edf)
+
+## Trouble starts here!
+## 1. Trying to say if response == "File" then we want the
+##    parameters to be a list of the parameters we already have + the files_schema
+##    parametrs (something like c(parameters, files_schema)), similarly if
+##    response == "Permission" the parameters should be c(parameters,
+##    permissions_schema)
+## 2. want to add $location to all of the schema parameters and set to "body"
+
+# edf <- endpoints %>%
+#   transpose(.names = nms) %>%
+#   simplify_all(.type = character(1)) %>%
+#   as_tibble() %>%
+#   mutate(response = response %>% map(1, .null = NA) %>% flatten_chr(),
+#          extra_params = ifelse(response == "File", files_schema, NA),
+#          all_params = list(list(parameters, extra_params)))
+# View(edf)
+
+# f <- function(x, thing, to_append) {
+#   if (x$response == thing) {
+#     nm <- names(x$parameters)
+#     nms <- names(to_append)
+#     names(to_append) <- paste(nm, nms, sep = ".")
+#     x <- x %>%
+#       mutate(extra_params = to_append)
+#   }
+#   x
+# }
+
+map(edf, f, thing = "File", to_append = files_schema)
+
+## clean up individual variables
+
+## docs omit the leading `drive`
+edf <- edf %>%
+  mutate(id = gsub("^drive\\.", "", id))
+
+
+## enforce my own order
+edf <- edf %>%
+  select(id, httpMethod, path, parameters, scopes, description, everything())
+
+edf$scopes <- edf$scopes %>%
+  map(~ gsub("https://www.googleapis.com/auth/", "", .)) %>%
+  map_chr(str_c, collapse = ", ")
+
+edf$parameterOrder <- edf$parameterOrder %>%
+  modify_if(~ length(.x) < 1, ~ NA_character_) %>%
+  map_chr(str_c, collapse = ", ")
+
+edf$response <- edf$response %>%
+  map_chr("$ref", .null = NA_character_)
+edf$request <- edf$request %>%
+  map_chr("$ref", .null = NA_character_)
+View(edf)
+
+## loooong side journey to clean up parameters
+## give them common sub-elements, in a common order
+params <- edf %>%
+  select(id, parameters) %>% {
+    ## unnest() won't work with a list ... doing it manually
+    tibble(
+      id = rep(.$id, lengths(.$parameters)),
+      parameters = purrr::flatten(.$parameters),
+      pname = names(parameters)
+    )
+  } %>%
+  select(id, pname, parameters)
+#params$parameters %>% map(names) %>% reduce(union)
+nms <-
+  c("location", "required", "type", "format", "description")
+
+## tibble with one row per parameter
+## variables method and pname keep track of endpoint and parameter name
+params <- params$parameters %>%
+  transpose(.names = nms) %>%
+  as_tibble() %>%
+  add_column(pname = params$pname, .before = 1) %>%
+  add_column(id = params$id, .before = 1)
+params <- params %>%
+  mutate(
+    location = location %>% flatten_chr(),
+    required = required %>% map(1, .null = NA) %>% flatten_lgl(),
+    type = type %>% flatten_chr(),
+    repeated = repeated %>% map(1, .null = NA) %>% flatten_lgl(),
+    format = format %>%  map(1, .null = NA) %>% flatten_chr(),
+    enum = enum %>%  modify_if(is_null, ~ NA),
+    description = description %>% flatten_chr()
+  )
+## repack all the info for each parameter into a list
+repacked <- params %>%
+  select(-id, -pname) %>%
+  pmap(list)
+params <- params %>%
+  select(id, pname) %>%
+  mutate(pdata = repacked)
+## repack all the parameters for each method into a named list
+params <- params %>%
+  group_by(id) %>%
+  nest(.key = parameters) %>%
+  mutate(parameters = map(parameters, deframe))
+
+## replace the parameters in the main endpoint tibble
+edf <- edf %>%
+  select(-parameters) %>%
+  left_join(params) %>%
+  select(id, httpMethod, path, parameters, everything())
+View(edf)
+
+
+
+## WE ARE DONE (THANK YOU JENNY!!)
+## saving in various forms
+
+## full spec as tibble, one row per endpoint
+out_fname <- str_replace(
+  json_fname,
+  "discovery-document.json",
+  "endpoints-tibble.rds")
+saveRDS(edf, file = out_fname)
+
+## full spec as list
+## transpose again, back to a list with one component per endpoint
+elist <- edf %>%
+  pmap(list) %>%
+  set_names(edf$id)
+listviewer::jsonedit(elist)
+
+out_fname <- str_replace(
+  json_fname,
+  "discovery-document.json",
+  "endpoints-list.rds")
+saveRDS(elist, file = out_fname)
+
+out_fname <- str_replace(
+  json_fname,
+  "discovery-document.json",
+  "endpoints-list.json")
+elist %>%
+  toJSON(pretty = TRUE) %>%
+  writeLines(out_fname)
+
+## partial spec as list, i.e. keep only the variables I currently use to
+## create the API
+## convert to my naming scheme, which is more consistent with general use
+.endpoints <- edf %>%
+  select(id, method = httpMethod, path, parameters) %>%
+  pmap(list) %>%
+  set_names(edf$id)
+listviewer::jsonedit(.endpoints)
+
+devtools::use_data(.endpoints, internal = TRUE, overwrite = TRUE)
