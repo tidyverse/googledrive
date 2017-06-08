bbe2eddeaeb3df3dd51de7ed2680d5b1564f6a39
jennybc <jenny@stat.ubc.ca>
2017-06-08 16:32:58 -0700

    Link to the Discovery Document

--- R/drive_endpoints.R
+++ R/drive_endpoints.R
@@ -4,7 +4,8 @@
 #' googledrive package. The names of this list (or the `id` sub-elements) are
 #' the nicknames that can be used to specify an endpoint in [build_request()].
 #' For each endpoint, we store its nickname or `id`, the associated HTTP
-#' verb, the `path`, and details about the parameters.
+#' verb, the `path`, and details about the parameters. This list is derived from
+#' the [Drive API v3 Discovery Document](https://www.googleapis.com/discovery/v1/apis/drive/v3/rest).
 #'
 #' @return A list containing the subset of the Drive API v3 endpoints that are
 #'   used internally by googledrive.
