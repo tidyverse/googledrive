796ee508bf90f8b9a80077d338ba863d74a36798
Lucy <ld.mcgowan@vanderbilt.edu>
2017-06-13 10:56:43 -0500

    replace drive_id() with as_id()

--- R/utils.R
+++ R/utils.R
@@ -21,36 +21,3 @@ last_all <- function(x) {
   Position(isTRUE, as.logical(cumprod(x)), right = TRUE, nomatch = 0)
 }
 
-## I expect the links to have /d/ before the fileId
-drive_extract_id <- function(x) {
-  id_loc <- regexpr("/d/([^/])+", x)
-  id <- ifelse(id_loc == -1, NA, gsub("/d/", "", regmatches(x, id_loc)))
-  id
-}
-
-drive_extract_id_smarter <- function(x) {
-  id <- drive_extract_id(x)
-  purrr::map2_chr(id, x, redirect_id)
-}
-
-redirect_id <- function(id, url) {
-  if (!is.na(id)) {
-    return(id)
-  }
-  header <- try(httr::HEAD(url), silent = TRUE)
-  if (class(header) == "try-error" || header$status_code > 400) {
-    return(id)
-  }
-  url <- header$url
-  drive_extract_id(url)
-}
-
-get_mime_type <- function(x) {
-  if (!(x %in% c("document", "spreadsheet", "folder", "presentation",
-                 "form", "drawing", "script"))) {
-    message(glue::glue("Input was not a Google Drive type: {x}"))
-    invisible(NULL)
-  } else {
-  paste0("application/vnd.google-apps.", x)
-  }
-}
