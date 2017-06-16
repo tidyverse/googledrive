572f545d9411602030f11cb761d6220669b23a56
Lucy D'Agostino McGowan <LucyMcGowan@users.noreply.github.com>
2017-06-16 12:06:49 -0500

    add tests for drive_mkdir() and drive_upload() folder logic (#98)
    * add tests for drive_mkdir() and drive_upload() folder logic
    
    * skip tests on travis & appveyor :see_no_evil:
    
    * create files within test

--- tests/testthat/test-drive_mkdir.R
+++ tests/testthat/test-drive_mkdir.R
@@ -1,35 +1,25 @@
-context("Upload files")
+context("Make folders")
 
 ## NOTE if you do not currently have the files needed,
 ## change run & clean below to TRUE to create files needed
 ## (CAUTION, this will delete files that will interfere)
 
-nm_ <- nm_fun("-TEST-drive-upload")
+nm_ <- nm_fun("-TEST-drive-mkdir")
 
 clean <- FALSE
-
 if (clean) {
   del <- drive_delete(c(nm_("foo"), nm_("bar")))
 }
 
-test_that("drive_upload() detects non-existant file", {
-  skip_on_appveyor()
-  skip_on_travis()
+test_that("drive_mkdir() places folder in the correct folder", {
 
-  #the file should be a file that exists
-  input <- "this should not work"
-  expect_error(drive_upload(from = input), "File does not exist")
-})
-
-test_that("drive_upload() places file in the correct folder", {
   skip_on_appveyor()
   skip_on_travis()
 
   foo <- drive_mkdir(nm_("foo"))
-
   ## foo
   ## |- bar
-  bar <- drive_upload(system.file("DESCRIPTION"), name = nm_("bar"), folder = nm_("foo"))
+  bar <- drive_mkdir(nm_("bar"), path = nm_("foo"))
   expect_identical(bar$files_resource[[1]]$parents[[1]], foo$id)
 
   ## clean up
