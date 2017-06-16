1b487cd5570e060443a85fad5a2ccdea3c5a8844
jennybc <jenny@stat.ubc.ca>
2017-06-16 16:58:59 +0200

    Work on help, tests

--- R/drive_mime_type.R
+++ R/drive_mime_type.R
@@ -1,9 +1,24 @@
-#' Lookup a mime type from a Google Drive type or file extension
+#' Lookup MIME type
 #'
-#' @param type Character. The Google Drive type or file extension you would
-#'   like to look up the mime type of.
+#' This is a helper to determinine which MIME type should be used for a file.
+#' Three specific types of input are acceptable:
+#' \itemize{
+#' \item Google Drive "native" file types. Important examples:
+#'   \itemize{
+#'   \item "document" for Google Docs
+#'   \item "folder" for folders
+#'   \item "presentation" for Google Slides
+#'   \item "spreadsheet" for Google Sheets
+#'   }
+#' \item File extensions, such as "pdf", "csv", etc.
+#' \item MIME types accepted by Google Drive (these are simply passed through).
+#' }
+#' If no input is provided, function returns the full tibble used for lookup,
+#' i.e. a tibble of MIME types known to be relevant to the Drive API.
 #'
-#' @return Character. The mime type of input.
+#' @param type Character. Google Drive file type, file extension, or MIME type.
+#'
+#' @return Character. MIME type.
 #'
 #' @examples
 #' ## get the mime type for Google Spreadsheets
@@ -11,35 +26,42 @@
 #'
 #' ## get the mime type for jpegs
 #' drive_mime_type("jpeg")
+#'
+#' ## it's vectorized
+#' drive_mime_type(c("presentation", "pdf", "image/gif"))
 #' @export
+#' @noMd
 drive_mime_type <- function(type = NULL) {
 
   if (is.null(type)) {
     return(.drive$mime_tbl)
   }
   if (!(is.character(type))) {
-    stop("`type` must be a character", call. = FALSE)
+    stop("`type` must be character", call. = FALSE)
   }
 
-  m <- match(
+  human_m <- match(
     type,
     .drive$mime_tbl$human_type,
     nomatch = NA_character_,
     incomparables = NA
   )
-  m <- ifelse(is.na(m),
-              match(type,
-                    .drive$mime_tbl$mime_type,
-                    nomatch = NA_character_,
-                    incomparables = NA
-              ),
-              m
+  ext_m <- match(
+    type,
+    .drive$mime_tbl$mime_type,
+    nomatch = NA_character_,
+    incomparables = NA
   )
+  m <- ifelse(is.na(human_m), ext_m, human_m)
   mime_type <- .drive$mime_tbl$mime_type[m]
 
   if (all(is.na(mime_type))) {
-    stop(glue::glue("We do not know a mime type for files of type: {sq(type)}"),
-         call. = FALSE)
+    stop(glue::glue(
+      "Unrecognized `type`:\n{problems}",
+      problems = glue::collapse(type[is.na(mime_type)], sep = "\n")
+      ),
+      call. = FALSE
+    )
   }
   mime_type
 }
--- tests/testthat/test-drive_mime_type.R
+++ tests/testthat/test-drive_mime_type.R
@@ -6,18 +6,39 @@ test_that("drive_mime_type() returns table if no input", {
   expect_gt(nrow(out), 0)
 })
 
-test_that("drive_mime_type() returns the a mime type if given a Google type",{
-  expect_identical(drive_mime_type(c("spreadsheet", "document")),
-               c("application/vnd.google-apps.spreadsheet",
-                 "application/vnd.google-apps.document"))
+test_that("drive_mime_type() returns MIME type for Drive native type",{
+  expect_identical(
+    drive_mime_type(c("spreadsheet", "document")),
+    c("application/vnd.google-apps.spreadsheet",
+      "application/vnd.google-apps.document")
+  )
 })
 
-test_that("drive_mime_type() returns the a mime type if given a file extension",{
-  expect_identical(drive_mime_type(c("pdf","jpeg")),
-               c("application/pdf", "image/jpeg"))
+test_that("drive_mime_type() returns MIME type for file extension",{
+  expect_identical(
+    drive_mime_type(c("pdf","jpeg")),
+    c("application/pdf", "image/jpeg")
+  )
 })
 
-test_that("drive_mime_type() errors if given nonsense",{
-  expect_error(drive_mime_type("nonsense"), "We do not know a mime type for files of type")
-  expect_error(drive_mime_type(1), "Please update `type` to be a character string.")
+test_that("drive_mime_type() returns MIME type for MIME type",{
+  input <- c("application/vnd.ms-excel","text/html")
+  expect_identical(drive_mime_type(input), input)
+})
+
+test_that("drive_mime_type() returns MIME type for mixed input",{
+  input <- c("text/html", NA, "folder", "csv")
+  expect_identical(
+    drive_mime_type(input),
+    c("text/html", NA, "application/vnd.google-apps.folder", "text/csv")
+  )
+})
+
+test_that("drive_mime_type() errors for invalid input",{
+  expect_error(drive_mime_type(1), "`type` must be character")
+  expect_error(drive_mime_type(dribble()), "`type` must be character")
+})
+
+test_that("drive_mime_type() errors for single unrecognized input",{
+  expect_error(drive_mime_type("nonsense"), "Unrecognized `type`")
 })
