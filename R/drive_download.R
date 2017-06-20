5e4675989dcf7cbcc900553a80100bb3de9d4829
Lucy <ld.mcgowan@vanderbilt.edu>
2017-06-19 21:28:11 -0400

    fix download for Google types

--- R/drive_download.R
+++ R/drive_download.R
@@ -1,64 +1,34 @@
 #' Download a file from Google Drive.
 #'
 #' @template file
-#' @param out_path Character. Path for output file. If `NULL`, we will
-#'    default to save a file to working directory by name of the Google
-#'    Drive file.
-#' @param type Character. If the file is a Google Drive type (Google Doc,
-#'    Google Sheet, Google Slide, etc.), you must specify the type of file
-#'    you would like to save (by file exension). This can be specified either
-#'    by using the `type` parameter, or by specifying the extension with the
-#'    `out_path` parameter.
+#' @param out_path Character. Path for output file. Google files can
+#'   be downloaded to types specified [here](https://developers.google.com/drive/v3/web/manage-downloads).
 #' @param overwrite A logical scalar, do you want to overwrite a file that already
 #'    exists locally?
 #' @template verbose
 #'
-#' @return An object of class `drive_file` containing raw data from
-#'   your downloaded Google Drive file.
-#'
 #' @examples
 #' \dontrun{
-#' ## save "chickwts.csv" to the working directory as "chickwts.csv".
-#' drive_download(file = "chickwts.csv")
+#' ## Save "chickwts.csv" to the working directory as "chickwts.csv".
+#' drive_download(file = "chickwts.csv", out_path = "chickwts.csv")
 #'
-#' ## save Google Sheet named "chickwts" to the working directory as
-#' ## chickwts.csv
-#' drive_download(file = "chickwts", out_file = "chickwts.csv")
-#' }
-#'
+#' ## Save Google Sheet named "chickwts" to the working directory as
+#' ## "chickwts.csv".
+#' drive_download(file = "chickwts", out_path = "chickwts.csv")
+#'}
 #' @export
 drive_download <- function(file = NULL,
                            out_path = NULL,
-                           type = NULL,
                            overwrite = FALSE,
                            verbose = TRUE) {
   file <- as_dribble(file)
   file <- confirm_single_file(file)
 
   if (is.null(out_path)) {
-    out_path <- file$name
+    stop("You must specify `out_path`.")
   }
 
   mime_type <- file$files_resource[[1]]$mimeType
-
-  if (!grepl("google", mime_type)) {
-
-  request <- generate_request(endpoint = "drive.files.get",
-                              params = list(alt = "media",
-                                            fileId = file$id))
-  } else {
-    ## TODO make sure the type is compatable.
-    type <- type %||% tools::file_ext(out_path)
-    if (type == "") {
-      ## TODO, be able to guess type by default (for example saving sheets
-      ## as csvs..)
-      stop("We cannot guess type yet")
-    }
-    mime_type <- drive_mime_type(type)
-    request <- generate_request(endpoint = "drive.files.export",
-                                params = list(fileId = file$id,
-                                              mimeType = mime_type ))
-  }
 
   ## it seems that it needs an extenstion to save properly
   ext <- tools::file_ext(out_path)
@@ -67,13 +37,27 @@ drive_download <- function(file = NULL,
     out_path <- paste0(out_path, ".", ext)
   }
 
+  if (!grepl("google", mime_type)) {
+
+  request <- generate_request(endpoint = "drive.files.get",
+                              params = list(alt = "media",
+                                            fileId = file$id))
+  } else {
+    ## TODO check that extension is actually allowed per
+    ## https://developers.google.com/drive/v3/web/manage-downloads
+    mime_type <- drive_mime_type(ext)
+    request <- generate_request(endpoint = "drive.files.export",
+                                params = list(fileId = file$id,
+                                              mimeType = mime_type ))
+  }
+
   response <- make_request(request, httr::write_disk(out_path, overwrite = overwrite))
-  success <- response$status_code == 200
+  success <- file.exists(out_path)
 
   if (success) {
     if (verbose) {
       message(
-        glue::glue("File downloaded from Google Drive:\n{file$name}\nSaved locally as:\n{out_path}")
+        glue::glue("File downloaded from Google Drive:\n{sq(file$name)}\nSaved locally as:\n{sq(out_path)}")
       )
     }
   } else {
