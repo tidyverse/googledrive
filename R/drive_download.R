a701436a0e4c93ecedcca503f8f4fd86b01d040b
Lucy <ld.mcgowan@vanderbilt.edu>
2017-06-26 13:10:23 -0500

    add type argument

--- R/drive_download.R
+++ R/drive_download.R
@@ -2,8 +2,10 @@
 #'
 #' @template file
 #' @param out_path Character. Path for output file. Will default to its Google Drive
-#'   name. Google files must include the intended extension and can be downloaded to types
+#'   name. Google files must include the intended extension. This can be specified by the
+#'   `out_path` or the `type` parameter. Google files can be downloaded to types
 #'   specified in the [Drive API documentation](https://developers.google.com/drive/v3/web/manage-downloads).
+#' @param type Character. Extension you would like for the local file.
 #' @param overwrite A logical scalar, do you want to overwrite a file that already
 #'    exists locally?
 #' @template verbose
@@ -17,13 +19,22 @@
 #' ## "chickwts.csv".
 #' drive_download(file = "chickwts", out_path = "chickwts.csv")
 #'
+#' ## This will also export a Google Sheet named "chickwts" to the working
+#' ## directory as "chickwts.csv".
+#' drive_download(file = "chickwts", type = "csv")
+#'
 #' ## Export a Google Document named "foobar" to the working directory as
-#' ## "foobar.docx"
+#' ## "foobar.docx".
 #' drive_download(file = "foobar", out_path = "foobar.docx")
+#'
+#' ## This will also export a Google Document named "foobar" to the working
+#' ## directory as "foobar.docx".
+#' drive_download(file = "foobar", type = "docx")
 #'}
 #' @export
 drive_download <- function(file = NULL,
                            out_path = NULL,
+                           type = NULL,
                            overwrite = FALSE,
                            verbose = TRUE) {
   file <- as_dribble(file)
@@ -32,6 +43,12 @@ drive_download <- function(file = NULL,
   out_path <- out_path %||% file$name
 
   mime_type <- file$files_resource[[1]]$mimeType
+
+
+  ext <- tools::file_ext(out_path)
+  if (ext == "" && !is.null(type)) {
+    out_path <- paste0(out_path, ".", type)
+  }
 
   if (grepl("google", mime_type)) {
     ## TODO check that extension is actually allowed per
