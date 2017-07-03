656aed49e3054251246d61e2c663215b83f69c6e
jennybc <jenny@stat.ubc.ca>
2017-07-03 08:15:17 -0700

    Reword `path` doc template

--- man-roxygen/path.R
+++ man-roxygen/path.R
@@ -1,9 +1,5 @@
-#' @param path Drive File, something that identifies where the file should live
-#'   on your Google Drive. Can be an actual path (character), a file id marked
-#'   with \code{\link{as_id}()}, or a \code{\link{dribble}}. If specified as an
-#'   actual path, you can include the name, indicated by not appending a final
-#'   slash (i.e.: "abc/def" would indicate the file will be named "def" and placed
-#'   in "abc", where as "abc/def/" indicates the file will be placed in folder "def"
-#'   which is located in folder "abc"), or you can specify the name with the
-#'   \code{name} parameter.
-
+#' @param path Specifies target destination for `file` on Google Drive. Can be
+#'   an actual path (character), a file id marked with \code{\link{as_id}()}, or
+#'   a \code{\link{dribble}}. If specified as an actual path, it is best to
+#'   explicitly indicate if it's a folder by including a trailing slash, since
+#'   it cannot always be worked out from the context of the call.
