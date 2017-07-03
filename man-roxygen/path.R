b42455f922ffc417cb8ae52ec60c2dbd469a0350
jennybc <jenny@stat.ubc.ca>
2017-07-03 10:20:02 -0700

    Tweak `path` doc template

--- man-roxygen/path.R
+++ man-roxygen/path.R
@@ -1,5 +1,6 @@
-#' @param path Specifies target destination for `file` on Google Drive. Can be
-#'   an actual path (character), a file id marked with \code{\link{as_id}()}, or
-#'   a \code{\link{dribble}}. If specified as an actual path, it is best to
-#'   explicitly indicate if it's a folder by including a trailing slash, since
-#'   it cannot always be worked out from the context of the call.
+#' @param path Specifies target destination for `file` or copy of `file` on
+#'   Google Drive. Can be an actual path (character), a file id marked with
+#'   \code{\link{as_id}()}, or a \code{\link{dribble}}. If specified as an
+#'   actual path, it is best to explicitly indicate if it's a folder by
+#'   including a trailing slash, since it cannot always be worked out from the
+#'   context of the call.
