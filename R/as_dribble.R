709d1069caddad13bcd2bff4077787e9e2e55ace
jennybc <jenny@stat.ubc.ca>
2017-07-20 20:08:04 -0700

    Work on as_dribble() help

--- R/as_dribble.R
+++ R/as_dribble.R
@@ -1,32 +1,34 @@
-#' Identify files on Google Drive.
+#' Identify files on Google Drive
 #'
-#' Converts various representations of Google Drive files into a [`dribble`],
-#' the object used by googledrive to hold Drive file metadata. Files can be
-#' specified via
-#'   * File path
-#'   * File id (be sure to mark with [as_id()] to distinguish from file path!)
+#' @description Converts various representations of Google Drive files into a
+#'   [`dribble`], the object used by googledrive to hold Drive file metadata.
+#'   Files can be specified via
+#'   * File path. File name is an important special case.
+#'   * File id. Mark with [as_id()] to distinguish from file path.
+#'   * Data frame or [`dribble`]. Once you've successfully used googledrive to
+#'     identify the files of interest, you'll have a [`dribble`]. Pass it into
+#'     downstream functions.
 #'   * List representing [Files resource](https://developers.google.com/drive/v3/reference/files#resource)
-#'     objects (for internal use)
-#'   * Data frame or [`dribble`] (for internal use)
+#'     objects. Mostly for internal use.
 #'
-#' For maximum clarity, always specify files via id or [`dribble`].
-#' If specifying via path, include trailing slash whenever describing a folder,
-#' for maximum clarity.
+#' @description This is a generic function.
 #'
-#' Some functions, such as [drive_cp()], [drive_mkdir()], [drive_mv()], and [drive_upload()]
-#' allow the specification of a `path` and/or `name`. If `name` is specified, `path` is
-#' expected to be a folder. If `path` is specified without a trailing slash, `name` is not
-#' specified, and a folder by the same name exists on Drive, the functions will error. If you
-#' intend `path` to be a folder, you must specify this by adding a trailing slash. If you
-#' intend to add a file with the same name as an existing folder, you must specify this by
-#' indicating the name in `name`.
+#' For maximum clarity, get your files into a [`dribble`] (or capture file id)
+#' as early as possible. When specifying via path, it's best to include the
+#' trailing slash when you're targetting a folder. If you want the folder `foo`,
+#' say `foo/`, not `foo`.
 #'
-#' This is a generic function.
-
+#' Some functions, such as [drive_cp()], [drive_mkdir()], [drive_mv()], and
+#' [drive_upload()], can accept the new file or folder name as the last part of
+#' `path`, when `name` is not given. But if you say `a/b/c` (no trailing slash)
+#' and a folder `a/b/c/` already exists, it's unclear what you want. A file
+#' named `c` in `a/b/` or a file with default name in `a/b/c/`? You get an
+#' error and must make your intent clear.
+#'
 #' @param x A vector of Drive file paths, a vector of file ids marked
 #'   with [as_id()], a list of Files Resource objects, or a suitable data
 #'   frame.
-#' @param ... Other arguments passed down to methods.
+#' @param ... Other arguments passed down to methods. (Not used.)
 #' @export
 #' @examples
 #' \dontrun{
