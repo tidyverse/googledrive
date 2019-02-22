ddd460a890c9440791fb06aa41ef80cfb377f99d
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-02-21 22:59:49 -0800

    JSON for updated Discovery Doc + new ingest script

---
+++ data-raw/discovery-doc-ingest.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+
+source(
+  system.file("discovery-doc-ingest", "ingest-functions.R", package = "gargle")
+)
+
+x <- download_discovery_document("drive:v3")
+dd <- read_discovery_document(x)
+
+methods <- get_raw_methods(dd)
+methods <- groom_methods(methods, dd)
+
+methods <- methods %>% map(add_schema_params, dd)
+methods <- methods %>% map(add_global_params, dd)
+
+## duplicate two methods to create a companion for media
+## simpler to do this here, in data, than in wrapper functions
+mediafy <- function(target_id, methods) {
+  new <- target_method <- methods[[target_id]]
+
+  new$id <- paste0(target_id, ".media")
+  new$path <-
+    pluck(target_method, "mediaUpload", "protocols", "simple", "path")
+  new$parameters <- c(
+    new$parameters,
+    uploadType = list(list(type = "string", required = TRUE, location = "query"))
+  )
+
+  methods[[new$id]] <- new
+  methods
+}
+
+methods <- mediafy("drive.files.update", methods)
+methods <- mediafy("drive.files.create", methods)
+
+.endpoints <- methods
+attr(.endpoints, "base_url") <- dd$rootUrl
+## View(.endpoints)
+
+usethis::use_data(.endpoints, internal = TRUE, overwrite = TRUE)
