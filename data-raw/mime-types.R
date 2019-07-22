83a7f70eeb8db2db2b6e45e1a61288238d0e4648
Jenny Bryan <jenny.f.bryan@gmail.com>
2019-07-22 16:37:26 -0700

    Remake table of MIME type translations

--- data-raw/mime-types.R
+++ data-raw/mime-types.R
@@ -1,46 +1,43 @@
 # mime-types tables
 # https://developers.google.com/drive/v3/web/mime-types
 # https://developers.google.com/drive/v3/web/manage-downloads
-library("tidyverse")
-library("httr")
-library("rvest")
-library("rprojroot")
-library("googledrive")
+library(tidyverse)
+library(httr)
+library(rvest)
+library(here)
+library(googledrive)
+
+conflicted::conflict_prefer("pluck", "purrr")
 
 ## MIME types for local file <--> Drive file
-fmts <- generate_request(
+fmts <- request_generate(
   endpoint = "drive.about.get",
   params = list(fields = "importFormats,exportFormats")
 ) %>%
   do_request()
 
-imports <- tibble::enframe(
-  fmts$importFormats,
-  name = "mime_type_local",
-  value = "mime_type_google"
-) %>%
-  mutate(
-    mime_type_google = purrr::simplify_all(mime_type_google),
-    action = "import"
+imports <- fmts %>%
+  pluck("importFormats") %>%
+  enframe(
+    name = "mime_type_local",
+    value = "mime_type_google"
   ) %>%
-  tidyr::unnest()
+  unnest_longer(mime_type_google) %>%
+  mutate(action = "import")
 
-exports <- tibble::enframe(
-  fmts$exportFormats,
-  name = "mime_type_google",
-  value = "mime_type_local"
-) %>%
-  mutate(
-    mime_type_local = purrr::simplify_all(mime_type_local),
-    action = "export"
+exports <- fmts %>%
+  pluck("exportFormats") %>%
+  enframe(
+    name = "mime_type_google",
+    value = "mime_type_local"
   ) %>%
-  tidyr::unnest()
+  unnest_longer(mime_type_local) %>%
+  mutate(action = "export")
 
 translate_mime_types <- bind_rows(imports, exports)
 
-defaults <- read_csv(
-  find_package_root_file("data-raw", "export-mime-type-defaults.csv")
-) %>%
+defaults <- here("data-raw", "export-mime-type-defaults.csv") %>%
+  read_csv() %>%
   mutate(
     action = "export",
     default = TRUE
@@ -48,15 +45,17 @@ defaults <- read_csv(
 
 translate_mime_types <- translate_mime_types %>%
   left_join(defaults) %>%
-  mutate(default = case_when(
-    action == "import" ~ NA,
-    is.na(default) ~ FALSE,
-    TRUE ~ TRUE
-  ))
+  mutate(
+    default = case_when(
+      action == "import" ~ NA,
+      is.na(default)     ~ FALSE,
+      TRUE               ~ TRUE
+    )
+  )
 
 write_csv(
   translate_mime_types,
-  path = find_package_root_file("inst", "extdata", "translate_mime_types.csv")
+  path = here("inst", "extdata", "translate_mime_types.csv")
 )
 
 ## general table of MIME types Google knows about
@@ -91,17 +90,18 @@ mime_tbl <- mime_ext %>%
     ext
   ))
 
-default_ext <- read_csv(
-  find_package_root_file("data-raw", "extension-mime-type-defaults.csv")
-) %>%
+default_ext <- here("data-raw", "extension-mime-type-defaults.csv") %>%
+  read_csv() %>%
   mutate(default = TRUE)
 
 mime_tbl <- mime_tbl %>%
   left_join(default_ext) %>%
-  mutate(default = case_when(
-    is.na(ext) ~ NA,
-    is.na(default) ~ FALSE,
-    TRUE ~ TRUE
-  ))
+  mutate(
+    default = case_when(
+      is.na(ext)     ~ NA,
+      is.na(default) ~ FALSE,
+      TRUE           ~ TRUE
+    )
+  )
 
-write_csv(mime_tbl, path = find_package_root_file("inst", "extdata", "mime_tbl.csv"))
+write_csv(mime_tbl, path = here("inst", "extdata", "mime_tbl.csv"))
