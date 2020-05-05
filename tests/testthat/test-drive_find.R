2e21f76674eeca06ab33e8f70c222629522e7e9c
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-05-05 08:04:53 -0700

    Make test more robust
    I've seen this a couple of times now:
    
    out$name has length 9, not length 10
    
    Getting "up to 10 files, in pages of size 5" usually returns 10 items, but sometimes less.

--- tests/testthat/test-drive_find.R
+++ tests/testthat/test-drive_find.R
@@ -81,7 +81,7 @@ test_that("drive_find() tolerates specification of pageSize", {
   expect_silent({
     out <- drive_find(n_max = 10, pageSize = 5, verbose = FALSE)
   })
-  expect_length(out$name, 10)
+  expect_lt(nrow(out), 10)
   expect_lt(anyDuplicated(out$id), 1)
 })
 
