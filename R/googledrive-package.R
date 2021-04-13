c1eee78c4b9083a7c84faaeaf93c1a6a0027d8bb
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-04-12 19:17:59 -0700

    Modernize package-level doc file

--- R/googledrive-package.R
+++ R/googledrive-package.R
@@ -11,7 +11,7 @@
 #'   The goal is to allow Drive access that feels similar to Unix file system
 #'   utilities, e.g., `find`, `ls`, `mv`, `cp`, `mkdir`, and `rm`.
 #'
-#'   The metadata for one or more Drive files is held in a `dribble`, a "Drive
+#'   The metadata for one or more Drive files is held in a [`dribble`], a "Drive
 #'   tibble". This is a data frame with one row per file. A dribble is returned
 #'   (and accepted) by almost every function in googledrive. It is designed to
 #'   give people what they want (file name), track what the API wants (file id),
@@ -21,25 +21,25 @@
 #'   require its use.
 #'
 #'   Please see the googledrive website for full documentation:
+
 #'   * <https://googledrive.tidyverse.org/index.html>
+
 #'
-#'   In addition to function-specific help, there are several articles which are
-#'   indexed here:
-#'   * [Article index](https://googledrive.tidyverse.org/articles/index.html)
+#' In addition to function-specific help, there are several articles which are
+#' indexed here:
+
+#' * [Article index](https://googledrive.tidyverse.org/articles/index.html)
+
 #'
-#' @importFrom rlang %||% :=
-#' @importFrom glue glue glue_data glue_collapse
-#' @importFrom lifecycle deprecated
 #' @keywords internal
 "_PACKAGE"
 
-
-## This function is never called
-## Exists to suppress this NOTE:
-## "Namespaces in Imports field not imported from:"
-## https://github.com/opencpu/opencpu/blob/10469ee3ddde0d0dca85bd96d2873869d1a64cd6/R/utils.R#L156-L165
-stub <- function() {
-  ## I have to use curl directly somewhere, if I import it.
-  ## I have to import it if I want to state a minimum version.
-  curl::curl_version()
-}
+## usethis namespace: start
+#' @importFrom glue glue
+#' @importFrom glue glue_collapse
+#' @importFrom glue glue_data
+#' @importFrom lifecycle deprecated
+#' @importFrom rlang :=
+#' @importFrom rlang %||%
+## usethis namespace: end
+NULL
