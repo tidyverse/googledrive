88c3e7eb303a7d67d8af05511375bfe485f6a291
Jenny Bryan <jenny.f.bryan@gmail.com>
2021-04-14 15:26:21 -0700

    Work around volatile bits of snapshot tests
    Test file names include the user, e.g., jenny vs. runner

--- tests/testthat/_snaps/drive_cp.md
+++ tests/testthat/_snaps/drive_cp.md
@@ -1,25 +1,24 @@
 # drive_cp() can copy file in place
 
     Code
-      file_cp <- drive_cp(file, name = me_("i-am-a-file"))
-    Message <simpleMessage>
+      writeLines(drive_cp_message)
+    Output
       File copied:
-        * i-am-a-file-TEST-drive-cp -> i-am-a-file-TEST-drive-cp-jenny
+        * i-am-a-file-TEST-drive-cp -> {cp_name}
 
 # drive_cp() can copy a file into a different folder
 
     Code
-      file_cp <- drive_cp(file, path = folder, name = me_("i-am-a-file"))
-    Message <simpleMessage>
+      writeLines(drive_cp_message)
+    Output
       File copied:
-        * i-am-a-file-TEST-drive-cp -> i-am-a-folder-TEST-drive-cp/i-am-a-file-TEST-drive-cp-jenny
+        * i-am-a-file-TEST-drive-cp -> i-am-a-folder-TEST-drive-cp/{cp_name}
 
 # drive_cp() takes name, assumes path is folder if both are specified
 
     Code
-      file_cp <- drive_cp(nm_("i-am-a-file"), path = nm_("i-am-a-folder"), name = me_(
-        "file-name"))
-    Message <simpleMessage>
+      writeLines(drive_cp_message)
+    Output
       File copied:
-        * i-am-a-file-TEST-drive-cp -> i-am-a-folder-TEST-drive-cp/file-name-TEST-drive-cp-jenny
+        * i-am-a-file-TEST-drive-cp -> i-am-a-folder-TEST-drive-cp/{cp_name}
 
--- tests/testthat/test-drive_mv.R
+++ tests/testthat/test-drive_mv.R
@@ -20,82 +20,128 @@ if (SETUP) {
 test_that("drive_mv() can rename file", {
   skip_if_no_token()
   skip_if_offline()
-  on.exit(drive_rm(me_("DESC-renamed")))
 
-  renamee <- drive_upload(system.file("DESCRIPTION"), me_("DESC"))
-
+  name_1 <- me_("DESC")
+  name_2 <- me_("DESC-renamed")
+  on.exit(drive_rm(name_2))
   local_drive_loud()
-  expect_snapshot(
-    out <- drive_mv(renamee, name = me_("DESC-renamed"))
+
+  file <- drive_upload(system.file("DESCRIPTION"), name_1)
+
+  drive_mv_message <- capture.output(
+    file <- drive_mv(file, name = name_2),
+    type = "message"
   )
-  expect_s3_class(out, "dribble")
-  expect_identical(nrow(out), 1L)
+  drive_mv_message <- sub(name_1, "{name_1}", drive_mv_message, perl = TRUE)
+  drive_mv_message <- sub(name_2, "{name_2}", drive_mv_message, perl = TRUE)
+  expect_snapshot(
+    writeLines(drive_mv_message)
+  )
+
+  expect_s3_class(file, "dribble")
+  expect_identical(nrow(file), 1L)
 })
 
 test_that("drive_mv() can move a file into a folder given as path", {
   skip_if_no_token()
   skip_if_offline()
-  on.exit(drive_rm(me_("DESC")))
 
-  movee <- drive_upload(system.file("DESCRIPTION"), me_("DESC"))
-
+  mv_name <- me_("DESC")
+  on.exit(drive_rm(mv_name))
   local_drive_loud()
+
+  mv_file <- drive_upload(system.file("DESCRIPTION"), mv_name)
+
   ## path is detected as folder (must have trailing slash)
-  expect_snapshot(
-    out <- drive_mv(movee, paste0(nm_("move-files-into-me"), "/"))
+  drive_mv_message <- capture.output(
+    mv_file <- drive_mv(mv_file, paste0(nm_("move-files-into-me"), "/")),
+    type = "message"
   )
-  expect_s3_class(out, "dribble")
-  expect_identical(nrow(out), 1L)
+  drive_mv_message <- gsub(mv_name, "{mv_name}", drive_mv_message, perl = TRUE)
+  expect_snapshot(
+    writeLines(drive_mv_message)
+  )
+
+  expect_s3_class(mv_file, "dribble")
+  expect_identical(nrow(mv_file), 1L)
   destination <- drive_get(nm_("move-files-into-me"))
-  expect_identical(out$drive_resource[[1]]$parents[[1]], destination$id)
+  expect_identical(mv_file$drive_resource[[1]]$parents[[1]], destination$id)
 })
 
 test_that("drive_mv() can move a file into a folder given as dribble", {
   skip_if_no_token()
   skip_if_offline()
-  on.exit(drive_rm(me_("DESC")))
 
-  movee <- drive_upload(system.file("DESCRIPTION"), me_("DESC"))
+  mv_name <- me_("DESC")
+  on.exit(drive_rm(mv_name))
+  local_drive_loud()
+
+  mv_file <- drive_upload(system.file("DESCRIPTION"), mv_name)
   destination <- drive_get(nm_("move-files-into-me"))
 
-  local_drive_loud()
-  expect_snapshot(
-    out <- drive_mv(movee, destination)
+  drive_mv_message <- capture.output(
+    mv_file <- drive_mv(mv_file, destination),
+    type = "message"
   )
-  expect_s3_class(out, "dribble")
-  expect_identical(nrow(out), 1L)
-  expect_identical(out$drive_resource[[1]]$parents[[1]], destination$id)
+  drive_mv_message <- gsub(mv_name, "{mv_name}", drive_mv_message, perl = TRUE)
+  expect_snapshot(
+    writeLines(drive_mv_message)
+  )
+
+  expect_s3_class(mv_file, "dribble")
+  expect_identical(nrow(mv_file), 1L)
+  expect_identical(mv_file$drive_resource[[1]]$parents[[1]], destination$id)
 })
 
 test_that("drive_mv() can rename and move, using `path` and `name`", {
   skip_if_no_token()
   skip_if_offline()
-  on.exit(drive_rm(me_("DESC-renamed")))
 
-  movee <- drive_upload(system.file("DESCRIPTION"), me_("DESC"))
-
+  name_1 <- me_("DESC")
+  name_2 <- me_("DESC-renamed")
+  on.exit(drive_rm(name_2))
   local_drive_loud()
-  expect_snapshot(
-    out <- drive_mv(movee, nm_("move-files-into-me"), me_("DESC-renamed"))
+
+  mv_file <- drive_upload(system.file("DESCRIPTION"), name_1)
+
+  drive_mv_message <- capture.output(
+    mv_file <- drive_mv(mv_file, nm_("move-files-into-me"), name_2),
+    type = "message"
   )
-  expect_s3_class(out, "dribble")
-  expect_identical(nrow(out), 1L)
+  drive_mv_message <- gsub(name_1, "{name_1}", drive_mv_message, perl = TRUE)
+  drive_mv_message <- gsub(name_2, "{name_2}", drive_mv_message, perl = TRUE)
+  expect_snapshot(
+    writeLines(drive_mv_message)
+  )
+
+  expect_s3_class(mv_file, "dribble")
+  expect_identical(nrow(mv_file), 1L)
 })
 
 test_that("drive_mv() can rename and move, using `path` only", {
   skip_if_no_token()
   skip_if_offline()
-  on.exit(drive_rm(me_("DESC-renamed")))
 
-  movee <- drive_upload(system.file("DESCRIPTION"), me_("DESC"))
-
+  name_1 <- me_("DESC")
+  name_2 <- me_("DESC-renamed")
+  on.exit(drive_rm(name_2))
   local_drive_loud()
-  expect_snapshot(
-    out <- drive_mv(
-      movee,
-      file.path(nm_("move-files-into-me"), me_("DESC-renamed"))
-    )
+
+  mv_file <- drive_upload(system.file("DESCRIPTION"), name_1)
+
+  drive_mv_message <- capture.output(
+    mv_file <- drive_mv(
+      mv_file,
+      file.path(nm_("move-files-into-me"), name_2)
+    ),
+    type = "message"
   )
-  expect_s3_class(out, "dribble")
-  expect_identical(nrow(out), 1L)
+  drive_mv_message <- gsub(name_1, "{name_1}", drive_mv_message, perl = TRUE)
+  drive_mv_message <- gsub(name_2, "{name_2}", drive_mv_message, perl = TRUE)
+  expect_snapshot(
+    writeLines(drive_mv_message)
+  )
+
+  expect_s3_class(mv_file, "dribble")
+  expect_identical(nrow(mv_file), 1L)
 })
