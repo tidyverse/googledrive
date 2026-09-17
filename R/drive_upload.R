fc3774d99d1f54d9aa6987afb3e809be4991624e
ekivett <erin.kivett03@gmail.com>
2026-09-17 14:01:19 -0500

    Grammar Fix for Drive Upload

--- R/drive_upload.R
+++ R/drive_upload.R
@@ -70,7 +70,7 @@
 #'   )
 #'
 #' # `overwrite = TRUE` moves the existing item to trash, then proceeds
-#' #Use `drive_update()` or `drive_put()` if you want to permissions and file is from an already existing file.
+#' #Use `drive_update()` or `drive_put()` if you want to update the permissions of an already existing file.
 #' chicken <- drive_example_local("chicken.jpg") |>
 #'   drive_upload(
 #'     name = "name-squatter-upload",
